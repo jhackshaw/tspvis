5797f495c9aec6f1ec178a596e83b2f4b45b827e
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2019-09-24 15:13:01 -0400

    best path always on top

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -42,5 +42,5 @@ export const selectPlotIntermediatePaths = createSelector(
 
 export const selectPlotPaths = createSelector(
   selectPlotBestPath, selectPlotIntermediatePaths,
-  (bestPath, intermediatePaths) => [bestPath, ...intermediatePaths]
+  (bestPath, intermediatePaths) => [...intermediatePaths, bestPath]
 )
