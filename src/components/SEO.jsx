f9977fa33876779f04bb8e5812fc781830006534
Jeff Hackshaw <jeffrey.hackshaw@gmail.com>
2019-10-18 11:13:47 -0400

    add opengraph tags

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 
+const description = "Interactive solver for the traveling salesman problem to visualize different algorithms. Includes various Heuristic and Exhaustive algorithms."
+
 const SEO = ({ subtitle }) => {
 
   return (
 
-    <Helmet title={`${subtitle} | Traveling Salesman Problem`}>
-      <meta name="description" content="Interactive solver for the traveling salesman problem to visualize different algorithms. Includes various Heuristic and Exhaustive algorithms." />
+    <Helmet title={`${subtitle} | Traveling Salesman Problem Visualizer`}>
+      <meta name="description" content={description} />
+
+      <meta property="og:title" content={`Traveling Salesman Problem Visualizer`} />
+      <meta property="og:url" content="https://tspvis.com" />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content="https://imgur.com/a/u3gUMUf" />
+      <meta property="og:type" content="website" />
     </Helmet>
   )
 }
